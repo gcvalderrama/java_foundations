6794e57695736eb8ba843886487bc6e2760bef91
Gregory Valderrama Vilca <gregory.valderrama@avantica.net>
2018-11-05 19:27:09 -0500

    add

--- aws_lambda/src/main/java/Api/MoneyHandler.java
+++ aws_lambda/src/main/java/Api/MoneyHandler.java
@@ -2,12 +2,32 @@ package Api;
 
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.io.IOUtils;
 
-public class MoneyHandler implements RequestHandler<MoneyRequest, MoneyResponse> {
 
-    public MoneyResponse handleRequest(MoneyRequest moneyRequest, Context context) {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+public class MoneyHandler implements RequestStreamHandler {
+
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        String result = "";
+        try {
+            result = IOUtils.toString(inputStream);
+        } catch (IOException e) {
+            throw e;
+        }
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
         MoneyResponse response = new MoneyResponse();
-        response.setKind(moneyRequest.getKind());
-        return response;
+        response.body = result;
+        response.headers.put("test_header", "test");
+        String ser = gson.toJson(response);
+        IOUtils.write(ser, outputStream, "UTF-8");
     }
 }
--- aws_lambda/src/main/java/Api/MoneyResponse.java
+++ aws_lambda/src/main/java/Api/MoneyResponse.java
@@ -1,12 +1,15 @@
 package Api;
 
-public class MoneyResponse {
-    String Kind;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 
-    public void setKind(String kind){
-        this.Kind = kind;
-    }
-    public String getKind(){
-        return this.Kind;
+public class MoneyResponse {
+    public String body;
+    public Dictionary<String,String> headers;
+
+    public MoneyResponse(){
+        this.headers = new Hashtable<String, String>();
     }
 }
